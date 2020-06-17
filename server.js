dd9b56dad986bc2ddbb17e57ba283fe69074c25f
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2020-06-17 18:41:01 +0530

    Nodemailer filer is uploaded

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
+const bodyPaser = require("body-parser");
+const app = express();
+
+//view engine
+app.set("view engine", ".hbs");
+app.engine(
+  "hbs",
+  exphbs({
+    extname: "hbs",
+    defaultLayout: "mainLayouts",
+    layoutsDir: __dirname + "/views/layouts/defaultlayouts",
+    partialsDir: __dirname + "/views/layouts/partiallayouts",
+  })
+);
+
+//body parser
+app.use(bodyPaser.urlencoded({ extended: false }));
+app.use(bodyPaser.json());
+
+app.get("/", (req, res) => {
+  res.render("./mail/home");
+});
+app.post("/", async (req, res) => {
+  console.log(req.body);
+  let output = `<p>Name : ${req.body.name}</p>
+  <p>Email: ${req.body.email}</p>
+  <p>Company: ${req.body.company}</p>
+  <p>Phone No: ${req.body.phone}</p>
+  <p>Message: ${req.body.message}</p>`;
+  
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "sh.mudassir80@gmail.com", // generated ethereal user
+      pass: "Shaik Mudassir80", // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Shaik 👻" <sh.mudassir80@gmail.com>', // sender address
+    to: `${req.body.email}`, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: output, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  res.render("./mail/home", { msg: "Email set....!" });
+});
+
+app.listen(3000, () => {
+  console.log("server started...! on port 3000");
+});
